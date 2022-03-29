10fb3c48f901864cd40f57c4258a8c9e8689c5da
duoduo <52656892+duoduozhifu@users.noreply.github.com>
2022-03-29 15:09:35 +0800

    Update zhoubao.py

--- zhoubao.py
+++ zhoubao.py
@@ -1,5 +1,5 @@
 import xlrd
-def zhoubao():
+def weekreport():
     path = "weekport.xlsx"
     wb = xlrd.open_workbook(f"{path}")
     sheet1 = wb.sheet_by_index(0)
@@ -18,10 +18,10 @@ def zhoubao():
             highriskvul = int(sheet2.cell_value(i, 2))
             medriskvul = int(sheet2.cell_value(i, 3))
             if (name1!=name2):
-                print(f'完成{name1} {count1}台主机基线扫描，其中不合规项数量{non_compliant}个，合规项数量{compliance}个，报告已发送给厂商，等待整改后复测。')
-                print(f'完成{name2} {count2}台主机漏洞扫描，其中高危漏洞{highriskvul}个，中危漏洞{medriskvul}个；报告已发送给厂商，等待整改后复测。')
+                print(f'。')
+                print(f'。')
 
             else:
-                print(f'完成{name1} {count1}台主机基线扫描，其中不合规项数量{non_compliant}个，合规项数量{compliance}个；完成漏洞扫描，其中高危漏洞{highriskvul}个，中危漏洞{medriskvul}个；报告已发送给厂商，等待整改后复测。')
+                print(f'。')
 
-zhoubao()
+weekreport()
